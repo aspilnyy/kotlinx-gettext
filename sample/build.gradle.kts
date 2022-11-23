775aee39dd2d186bdb600651b3d3ed170ed00670
anton.spilnyy <anton.spilnyy@jetbrains.com>
2022-11-23 08:42:37 +0100

    add dependency to IR and change version

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("jvm")
-    id("name.kropp.kotlinx-gettext") version "0.4.1-dev-1"
+    id("name.kropp.kotlinx-gettext") version "0.4.1-dev-1-ir"
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("name.kropp.kotlinx-gettext:kotlinx-gettext-jvm:0.4.1-dev-1")
+    implementation("name.kropp.kotlinx-gettext:kotlinx-gettext-jvm:0.4.1-dev-1-ir")
 }
 
 gettext {
